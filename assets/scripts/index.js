c495f2ac4c87b72713b412d6a712b127db6dd150
dimasaulia <dimasauliafachrudin@gmail.com>
2021-10-31 15:06:57 +0700

    Menambahkan Navigasi Bar Baru Serta Halaman Utama Perpustakaan

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $("#ikon").click(function(){
         $( "#cari" ).trigger( "click" );
     });
+
+    $("#iconMenu").click(function(){
+        $("menu").toggleClass("close");  
+        console.log("Hallo");
+    })
 });
